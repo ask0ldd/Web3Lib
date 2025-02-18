1678fad82035af85c3e960ab7b6b16aff11acccb
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-02-18 10:28:10 +0100

    *add* deployed tokens list

---
+++ src/constants/deployedTokens.ts
@@ -0,0 +1,15 @@
+export const hhTokens = [
+    { name : 'CrystalDrive', symbol : 'CRD', contractAddress : '0x5FbDB2315678afecb367f032d93F642f64180aa3' },
+    { name : 'CyberSpark', symbol : 'CSP', contractAddress : '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512' },
+    { name : 'EchoChain', symbol : 'ECH', contractAddress : '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0' },
+    { name : 'NeoNova', symbol : 'NOV', contractAddress : '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9' },
+    { name : 'NimbusNet', symbol : 'NMB', contractAddress : '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9' },
+    { name : 'PrimeFlow', symbol : 'PRM', contractAddress : '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707' },
+    { name : 'Quantum', symbol : 'QTM', contractAddress : '0x0165878A594ca255338adfa4d48449f69242Eb8F' },
+    { name : 'StellarPulse', symbol : 'STP', contractAddress : '0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6' },
+    { name : 'VertexCoin', symbol : 'VTX', contractAddress : '0x8A791620dd6260079BF849Dc5567aDC3F2FdC318' },
+    { name : 'ZenithToken', symbol : 'ZNT', contractAddress : '0x610178dA211FEF7D417bC0e6FeD39F05609AD788' },
+    { name : 'SolarFlare', symbol : 'SFL', contractAddress : '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853' }, // free from any allowance by default
+] as const
+
+export default hhTokens
